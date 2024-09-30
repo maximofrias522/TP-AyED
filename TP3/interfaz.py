9af3b27d0594c68030d6dfe3140ce21aa4db8fe1
maximo frias <129902079+maximofrias522@users.noreply.github.com>
2024-09-29 21:17:22 -0300

    Modificaciones iniciales en interfaz

--- TP3/interfaz.py
+++ TP3/interfaz.py
@@ -1,4 +1,7 @@
 import os
+from rich.console import Console
+
+console = Console()
 
 asciiart = '''
  █████  ████████████████ ██████   █████    ██████   ██████           █████            █████     ███
@@ -13,7 +16,7 @@ asciiart = '''
 
 def limpiarPantalla():
     os.system('cls' if os.name == 'nt' else 'clear') # si el sistema es win usa el comando cls, en caso contrario asume que es unix y usa clear
-    print(asciiart)
+    console.print(asciiart, justify='center')
 
 def continuar():
     input("Presione enter para continuar...")
@@ -22,28 +25,48 @@ def opcionInvalida():
     print("Error: la opcion ingresada no es valida.")
     continuar()
 
-def mostrarGestionarPerfil():
+def menuInicial():
     limpiarPantalla()
-    print("Estas gestionando tu perfil")
-    print("a. Editar mis datos personales")
-    print("b. Ver mi perfil")
-    print("c. Eliminar mi perfil")
-    print("d. Volver")
+    console.print('[bold white]Bienvenido![/bold white]', justify='center')
+    console.print('1. Iniciar sesión', justify='center')
+    console.print('2. Registrarse', justify='center')
+    console.print('0. [red]Salir[/red]', justify='center')
 
-def mostrarMenuEstudiante(nombre):
+def registrarse():
     limpiarPantalla()
-    print("Bienvenido, ", nombre)
-    print("Menú Principal:")
-    print("1. Gestionar mi perfil")
-    print("2. Gestionar candidatos")
-    print("3. Matcheos")
-    print("4. Reportes estadísticos")
-    print("5. Bonus track 1")
-    print("6. Bonus track 2")
-    print("0. Salir")
+    console.print('1. Registrarse como estudiante', justify='center')
+    console.print('2. Registrarse como moderador', justify='center')
+    console.print('0. Volver', justify='center')
 
-def mostrarMenuInicial():
+def iniciarSesion():
     limpiarPantalla()
-    print('1. Iniciar sesión')
-    print('2. Registrarse')
-    print('0. Salir')
+    console.print('[bold white]Por favor ingrese los datos de su cuenta[/bold white]', justify='center')
+
+def menuEst():
+    limpiarPantalla()
+    console.print('[bold white]Bienvenido a UTN MATCH![/bold white]', justify='center')
+    console.print('1. Gestionar mi perfil', justify='center')
+    console.print('2. Gestionar candidatos', justify='center')
+    console.print('3. Matcheos' , justify='center')
+    console.print('4. Reportes estadisticos', justify='center')
+    console.print('0. [red]Salir[/red]', justify='center')
+
+def gestionarPerfil():
+    limpiarPantalla()
+    console.print('[bold white]Estas gestionando tu perfil[/bold white]', justify='center')
+    console.print('1. Editar mis datos personales', justify='center')
+    console.print('2. Ver mi perfil', justify='center')
+    console.print('3. Eliminar mi perfil', justify='center')
+    console.print('0. Volver', justify='center')
+
+def editarDatos():
+    console.print('[bold white]Estas editando tus datos personales[/bold white]', justify='center')
+    console.print('[bold white]¿Qué datos deseas editar?[/bold white]', justify='center')
+    console.print('1. Fecha de nacimiento', justify='center')
+    console.print('2. Biografia', justify='center')
+    console.print('3. Hobbies', justify='center')
+    console.print('0. Volver')
+
+def 
+
+menuEst()
